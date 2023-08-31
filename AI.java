9bdcdf0213222b02fe537a2793e5478b2c51ebb4
Faddi Azzam Dabain <96977807+FaddiADabain@users.noreply.github.com>
2023-08-30 21:46:29 -0400

    Create AI.java

---
+++ AI.java
@@ -0,0 +1,24 @@
+public class AI {
+	private int row;
+	private int col;
+	
+	public void setRow(int row)
+	{
+		this.row = row;
+	}
+	
+	public void setCol(int col)
+	{
+		this.col = col;
+	}
+	
+	public int getRow()
+	{
+		return row;
+	}
+	
+	public int getCol()
+	{
+		return col;
+	}
+}
